8ca25e2dad65f4e610edb86b111c085c4a18917b
zetanove <antonio.pelleriti@gmail.com>
2022-03-12 16:19:00 +0100

    esempio worker process name

--- capitolo 02/EmptyWebApp/Program.cs
+++ capitolo 02/EmptyWebApp/Program.cs
@@ -1,6 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Ciao mondo 2!");
+app.MapGet("/", () => "Ciao mondo!");
+
+app.MapGet("/info", 
+    () => "Worker Process Name: "+
+        System.Diagnostics.Process.GetCurrentProcess().ProcessName);
+
 
 app.Run();
