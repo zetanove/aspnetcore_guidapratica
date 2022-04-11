07aa634bb06638e6e9c23ca5bfddc78a9b4d3f73
zetanove <antonio.pelleriti@gmail.com>
2022-04-12 01:25:13 +0200

    view examples

---
+++ capitolo 05/TodoWebApp/TodoWebApp/Controllers/TodoController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TodoWebApp.Controllers
+{
+    public class TodoController : Controller
+    {
+        // GET: TodoController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: TodoController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: TodoController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TodoController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TodoController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: TodoController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TodoController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: TodoController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ capitolo 05/TodoWebApp/TodoWebApp/Models/Todo.cs
@@ -0,0 +1,22 @@
+﻿namespace TodoWebApp.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Titolo { get; set; }
+        public string Descrizione { get; set; }
+        public DateTime Promemoria { get; set; }
+        public bool Completato { get; set; }
+
+        public string Category { get; set; }
+    }
+
+    public class CategoryService
+    {
+        public List<string> GetCategories()
+        {
+            return new List<string>() { "Rosso", "Giallo", "Verde" };
+        }
+
+    }
+}
